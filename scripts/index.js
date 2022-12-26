0fe7cda1183c8f0cd05bf5491085d667df45ccdf
Olga Pomytkina <olpoma@yandex.ru>
2022-12-26 21:30:11 +0300

    feat: написать код для Popup

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,25 @@ closeEditButton.addEventListener('click', (event) => {
   popupProfile.classList.remove('popup_opened');
 })
 
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('#profile-name');
+let jobInput = formElement.querySelector('#profile-occupation');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__occupation');
 
+function closePopup(popupName) {
+  popupName.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  jobInput.value;
+  closePopup(popupProfile);
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
 
 
 
