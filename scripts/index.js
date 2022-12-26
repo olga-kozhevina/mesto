dd7f28fbaeb662264bc68602fd2b58c707cde4b5
Olga Pomytkina <olpoma@yandex.ru>
2022-12-26 23:38:12 +0300

    fix: исправлено соглансо комментариям ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,10 @@ const popupProfile = document.querySelector('.popup');
 const closeEditButton = popupProfile.querySelector('.popup__close-button');
 
 openEditButton.addEventListener('click', (event) => {
-  console.log(event);
   popupProfile.classList.add('popup_opened');
 })
 
 closeEditButton.addEventListener('click', (event) => {
-  console.log(event);
   popupProfile.classList.remove('popup_opened');
 })
 
@@ -18,16 +16,11 @@ let jobInput = formElement.querySelector('#profile-occupation');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__occupation');
 
-function closePopup(popupName) {
-  popupName.classList.remove('popup_opened');
-}
-
 function handleFormSubmit (event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  jobInput.value;
-  closePopup(popupProfile);
+  closeEditButton;
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
