665cb2621e89ec70c94c8aa172a86fe8192bfea7
Olga Pomytkina <olpoma@yandex.ru>
2022-12-28 03:26:24 +0300

    fix: исправления согласно комментариям ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,40 @@
 const openEditButton = document.querySelector('.profile__edit-button');
-const popupProfile = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_type_profile');
 const closeEditButton = popupProfile.querySelector('.popup__close-button');
 
-openEditButton.addEventListener('click', (event) => {
-  popupProfile.classList.add('popup_opened');
-})
+openEditButton.addEventListener('click', editProfile);
+
+function openPopup(popupName) {
+  popupName.classList.add('popup_opened');
+}
+
+function editProfile() {
+  openPopup(popupProfile);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
 
 closeEditButton.addEventListener('click', (event) => {
-  popupProfile.classList.remove('popup_opened');
-})
+  closePopup(popupProfile)
+});
+
+function closePopup(popupName) {
+  popupName.classList.remove('popup_opened');
+}
 
 let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('#profile-name');
-let jobInput = formElement.querySelector('#profile-occupation');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_occupation');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__occupation');
 
-function handleFormSubmit (event) {
+
+function handleFormSubmit(event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closeEditButton;
+  closePopup(popupProfile);
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
-
-
 
